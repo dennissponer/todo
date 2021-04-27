ed4c473a12381df1abc09c7fdec2a08441b113fe
Dennis M Sponer <76220601+dennissponer@users.noreply.github.com>
2021-04-26 17:03:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-function App(){
-  const [todos, setTodos] = React.useState([
-    {
-      text: 'learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'meet friend for lunch',
-      isCompleted: false,
-    },
-    {
-      text: 'build todo app',
-      isCompleted: false,
-    }        
-  ]);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'
 
-  const addTodo = text => {
-    const newTodos = [...todos, {text, isCompleted:false}];
-    setTodos(newTodos);
-  }
-  const removeTodo = index => {
-    let temp = [...todos];    
-    temp.splice(index, 1);
-    setTodos(temp);
-  }
-
-  return(
-    <div className="app">
-      <div className="todo-list" >
-        {todos.map((todo, i) => (
-          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
